b767650bef84806d892d1ab6cae7dc5d3e4dc69e
Jackson Ly <48436180+JumpyJacko@users.noreply.github.com>
2023-01-27 10:39:24 +1100

    Added input (partial)

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
+use console::{Key, Term};
 
-const BOARD_SIZE: usize = 8;
+const SIZEY: usize = 8;
+const SIZEX: usize = 8;
 
-type Point = (u32, u32);
-type Maze = [[usize; BOARD_SIZE]; BOARD_SIZE];
+type Point = (usize, usize);
+type Maze = [[usize; SIZEX]; SIZEY];
 
 enum Direction {
     Right,
     Left,
     Down,
-    Up
+    Up,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct Entity {
-    name: String,
+    name: &'static str,
     position: Point,
 }
 
@@ -27,53 +29,90 @@ struct State {
 const TILES: [&str; 4] = ["..", "##", "\x1b[34m@\x1b[0m.", "\x1b[42m..\x1b[0m"];
 
 impl Entity {
-    fn new(name: String, position: Point) -> Entity {
-        Entity {
-            name,
-            position
-        }
+    fn new(name: &'static str, position: Point) -> Entity {
+        Entity { name, position }
     }
 
-    fn move_entity(&self, position: Direction) -> Entity {
-        match position {
-            Direction::Right => todo!(),
-            Direction::Left => todo!(),
-            Direction::Down => todo!(),
-            Direction::Up => todo!(),
+    fn move_entity(&self, direction: Direction) -> Entity {
+        match direction {
+            Direction::Right => Entity {
+                name: self.name,
+                position: (self.position.0 + 1, self.position.1),
+            },
+            Direction::Left => Entity {
+                name: self.name,
+                position: (self.position.0 - 1, self.position.1),
+            },
+            Direction::Down => Entity {
+                name: self.name,
+                position: (self.position.0, self.position.1 + 1),
+            },
+            Direction::Up => Entity {
+                name: self.name,
+                position: (self.position.0, self.position.1 - 1),
+            },
         }
     }
 }
 
 impl State {
     fn new(mut maze: Maze, spawn: Point) -> State {
-        maze[spawn.1 as usize][spawn.0 as usize] = 2;
+        maze[spawn.1][spawn.0] = 2;
 
         State {
             maze,
-            player: Entity::new("Player".to_owned(), spawn),
+            player: Entity::new("Player", spawn),
         }
     }
 
-    fn update(&self) -> State {
-        todo!();
+    fn input_and_update(&self, p_input: Direction) -> State {
+        let new_player: Entity;
+        match p_input {
+            Direction::Right => {
+                if self.check_valid(self.player.move_entity(Direction::Right)) {
+                    new_player = self.player.move_entity(Direction::Right)
+                } else {new_player = self.player}
+            }
+            Direction::Left => {
+                if self.check_valid(self.player.move_entity(Direction::Left)) {
+                    new_player = self.player.move_entity(Direction::Left)
+                } else {new_player = self.player}
+            }
+            Direction::Down => {
+                if self.check_valid(self.player.move_entity(Direction::Down)) {
+                    new_player = self.player.move_entity(Direction::Down)
+                } else {new_player = self.player}
+            }
+            Direction::Up => {
+                if self.check_valid(self.player.move_entity(Direction::Up)) {
+                    new_player = self.player.move_entity(Direction::Up)
+                } else {new_player = self.player}
+            }
+        };
+
+        println!("{:?} \n{:?}", new_player, self.player);
+
+        State {
+            maze: self.maze,
+            player: new_player,
+        }
     }
 
     fn render(&self) {
         // print!("\x1B[1;1H");
-        /*
-        Tileset will probably be,
-           0 = .  | 1 = ## | 2 = @  | 3 = M  
-           - read from State/self
-           - use numbers in State/self to index the tileset
-           - render & profit!
-         */
-
         self.maze.iter().for_each(|row| {
             row.iter().for_each(|c| {
                 print!("{}", TILES[*c]);
             });
             println!();
         });
+    }
+
+    fn check_valid(&self, entity: Entity) -> bool {
+        match self.maze[entity.position.1][entity.position.0] {
+            0 => true,
+            _ => false,
+        }
     }
 
     fn check_win_state(&self) -> bool {
@@ -83,14 +122,14 @@ impl State {
     fn to_adj_neighbours(&self) -> Vec<Vec<usize>> {
         todo!();
     }
-    
+
     fn from_adj_neighbours(nodes: Vec<Vec<usize>>) -> State {
         todo!();
     }
 }
 
 fn main() {
-    // TODO: Procedurally generate a maze using the Depth-First Search (DFS) method to make 
+    // TODO: Procedurally generate a maze using the Depth-First Search (DFS) method to make
     //       an adjacent neighbours table to then translate into a 2D array like this one
     let maze: Maze = [
         [1, 1, 1, 1, 1, 1, 1, 1],
@@ -107,4 +146,20 @@ fn main() {
 
     // print!("\x1B[2J\x1B[1;1H");
     state.render();
+
+    let stdout = Term::buffered_stdout();
+    'game_loop: loop {
+        if let Ok(key) = stdout.read_key() {
+            match key {
+                Key::ArrowLeft  => state = state.input_and_update(Direction::Left),
+                Key::ArrowRight => state = state.input_and_update(Direction::Right),
+                Key::ArrowUp    => state = state.input_and_update(Direction::Up),
+                Key::ArrowDown  => state = state.input_and_update(Direction::Down),
+                Key::Escape     => break 'game_loop,
+                _ => break 'game_loop, // Handle later
+            };
+
+            state.render();
+        }
+    }
 }
